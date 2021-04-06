1b1688c6efdde36813372e753594485ca562fc75
Marcelo junior <skowiaki@gmail.com>
2021-04-05 21:31:47 -0300

    Rework(Model): Melhorias na classe model
    Melhoria no retorno do método get  convertendo a resposta da chamada de Json para list/map para facilitar sua utilização no código

--- mobile/lib/model/modelClass.dart
+++ mobile/lib/model/modelClass.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/cupertino.dart';
 
@@ -9,12 +10,14 @@ class Model {
 
   ///no metodo get devemos encaminhar uma header composta com um map
   Future get({@required Map<String, String> header}) async {
-    var response;
+    Map response = {};
 
     try {
-      response = await http.get(url, headers: header);
+      var apiResponse = await http.get(url, headers: header);
+      response["status"] = apiResponse.statusCode;
+      response["body"] = jsonDecode(apiResponse.body);
     } catch (e) {
-      response = {"status": "erro", "error": e};
+      response = {"status": 1, "body": e};
     }
     return response;
   }
